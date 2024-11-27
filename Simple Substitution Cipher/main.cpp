7b95c44d7ab36421df46dcf4b65f12050c61609e
ahmed Elsherif <162807418+ahmed2005hussen@users.noreply.github.com>
2024-11-27 02:17:53 +0200

    Simple Substitution Cipher.cpp

--- Simple Substitution Cipher/main.cpp
+++ Simple Substitution Cipher/main.cpp
@@ -1,85 +1,47 @@
+// program name : Simple Substitution Cipher
+// programmer : ahmed hussein ahmed mohammed salh
+// version 1
+// Data: 9 march 2023
+
 #include <iostream>
 #include <cctype>
 #include <string>
-#include <bitset>
-#include <algorithm>
-#include <iomanip>
 using namespace std;
 
 string sentence, key;
 
-
-int q = 0 ;
-int punction()
-{
+int punction() {
     for (int i = 0; i < key.size(); ++i) {
-
-
         if (ispunct(key[i])) {
             return 1;
         }
-
     }
     return 0;
 }
 
-
-
-
-int digit()
-{
+int digit() {
     for (int i = 0; i < key.size(); ++i) {
-
-
         if (isdigit(key[i])) {
             return 1;
         }
-
     }
     return 0;
 }
-
-
 int uniqe() {
-
     for (int i = 0; i < key.size(); ++i) {
-
         for (int k = i + 1; k < key.size(); ++k) {
-
             if (key[i] == key[k]) {
                 return 1;
             }
         }
     }
-
     return 0;
 }
-
-int simple()
-{
-
-    /*
-
-   Author : Ahmed Hussein Ahmed Mohammed
-
-
-   program name:  Simple Substitution Cipher.
-
-
-   purpose : encryption and decryption
-
-   Date : March 29 , 2024
-
-   Version 3
-
-    */
-
-
+int main() {
     cout << "****************************************************" << endl;
     cout << "************ Simple Substitution Cipher ************" << endl;
     cout << "****************************************************" << endl;
     cout << endl;
-
 
     string str = "abcdefghijklmnopqrstuvwxyz";
     int process;
@@ -89,144 +51,102 @@ int simple()
     cout << "What do you like to do today?" << endl;
     cout << endl;
 
-    while (true)
-    {
+    while (true) {
         cout << "1- Cipher a message" << endl;
         cout << "2- Decipher a message" << endl;
         cout << "3- End" << endl;
         cin >> process;
         cin.ignore();
-        if (process == 1)
-        {
+        if (process == 1) {
             cout << "Enter the sentence which do you want cipher it ";
             getline(cin, sentence);
-            // to make the letters lowercase
-            for (char& c : sentence) {
+
+            for (char &c: sentence) {
                 c = tolower(c);
             }
-
             cout << "Enter the key , key of 5 unique letters ";
             while (true) {
                 cin >> key;
-                // to make the letters lowercase
-                for (char& c : key) {
+
+                for (char &c: key) {
                     c = tolower(c);
                 }
-                // to ensure that the key is uniqe and does not contain a number or punctuaion
-                if (key.size() == 5 && !uniqe() && !digit() && !punction())
-                {
 
-                    for (int i = 0; i < 26; i++)
-                    {
-                        if (char(i + 'a') == key[0])
-                        {
+                if (key.size() == 5 && !uniqe() && !digit() && !punction()) {
+
+                    for (int i = 0; i < 26; i++) {
+                        if (char(i + 'a') == key[0]) {
                             continue;
                         }
-                        if (char(i + 'a') == key[1])
-                        {
+                        if (char(i + 'a') == key[1]) {
                             continue;
                         }
-                        if (char(i + 'a') == key[2])
-                        {
+                        if (char(i + 'a') == key[2]) {
                             continue;
                         }
-                        if (char(i + 'a') == key[3])
-                        {
+                        if (char(i + 'a') == key[3]) {
                             continue;
                         }
-                        if (char(i + 'a') == key[4])
-                        {
+                        if (char(i + 'a') == key[4]) {
                             continue;
-                        }
-                        else
-                        {
+                        } else {
                             key += char(i + 'a');
                         }
-
-
-
                     }
-
-                    for (int i = 0; i < sentence.size(); i++)
-                    {
+                    for (int i = 0; i < sentence.size(); i++) {
                         for (int x = 0; x < 26; x++) {
                             if (sentence[i] == str[x]) {
                                 sentence[i] = key[x];
                                 break;
                             }
                         }
-
                     }
                     cout << "The sentence is : " << sentence << endl;
-
                     break;
-
-                }
-                else {
+                } else {
                     cout << "Pls enter valid key : ";
                     continue;
                 }
-
             }
-        }
-
-
-
-
-
-
-        else if (process == 2)
-        {
-
+        } else if (process == 2) {
             cout << "Enter the sentence which do you want decipher it ";
             getline(cin, sentence);
 
-            for (char& c : sentence) {
+            for (char &c: sentence) {
                 c = tolower(c);
             }
             cout << "Enter the key , key of 5 unique letters ";
             while (true) {
                 cin >> key;
 
-                for (char& c : key) {
+                for (char &c: key) {
                     c = tolower(c);
                 }
 
-                if (key.size() == 5 && !uniqe() && !digit() && !punction())
-                {
+                if (key.size() == 5 && !uniqe() && !digit() && !punction()) {
 
-                    for (int i = 0; i < 26; i++)
-                    {
-                        if (char(i + 'a') == key[0])
-                        {
+                    for (int i = 0; i < 26; i++) {
+                        if (char(i + 'a') == key[0]) {
                             continue;
                         }
-                        if (char(i + 'a') == key[1])
-                        {
+                        if (char(i + 'a') == key[1]) {
                             continue;
                         }
-                        if (char(i + 'a') == key[2])
-                        {
+                        if (char(i + 'a') == key[2]) {
                             continue;
                         }
-                        if (char(i + 'a') == key[3])
-                        {
+                        if (char(i + 'a') == key[3]) {
                             continue;
                         }
-                        if (char(i + 'a') == key[4])
-                        {
+                        if (char(i + 'a') == key[4]) {
                             continue;
-                        }
-                        else
-                        {
+                        } else {
                             key += char(i + 'a');
                         }
 
 
-
                     }
-                    for (int i = 0; i < sentence.size(); i++)
-                    {
+                    for (int i = 0; i < sentence.size(); i++) {
                         for (int x = 0; x < 26; x++) {
                             if (sentence[i] == key[x]) {
                                 sentence[i] = str[x];
@@ -238,55 +158,18 @@ int simple()
                     cout << "The sentence is : " << sentence << endl;
                     break;
 
-                }
-                else {
+                } else {
                     cout << "Pls enter valid key : ";
                     continue;
                 }
-
-
-
-
-
-
             }
 
-        }
-        else if (process == 3)
-        {
-            cout << " Good bye :) " << endl;
-            cout << endl;
-            q+=1;
+        } else if (process == 3) {
+            cout << " Good bye :)";
             break;
-        }
-        else
-        {
+        } else {
             cout << "Please choice number from 1 To 3 " << endl;
             continue;
         }
-
-
-
-    }
-
-    return 0;
-}
-
-
-
-
-
-
-int main() {
-    while (true)
-    {
-        if (q==1)
-        {
-            break;
-        }
-
-        simple();
     }
 }
-
-
